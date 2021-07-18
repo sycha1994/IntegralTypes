c0bd298826983cb28cfcac2fa14645d338505cf1
sycha1994 <sycha1994@gmail.com>
2021-07-18 23:16:24 +0900

    4

--- MainApp.cs
+++ MainApp.cs
@@ -6,21 +6,16 @@ namespace IntegralTypes
     {
         static void Main(string[] args)
         {
-            int a = 123;
-            string b = a.ToString();
-            Console.WriteLine(b);
+            int? a = null;
 
-            float c = 3.14f;
-            string d = c.ToString();
-            Console.WriteLine(d);
+            Console.WriteLine(a.HasValue);
+            Console.WriteLine(a != null);
 
-            string e = "123456";
-            int f = Convert.ToInt32(e);
-            Console.WriteLine(f);
+            a = 3;
 
-            string g = "1.2345";
-            float h = float.Parse(g);
-            Console.WriteLine(h);
+            Console.WriteLine(a.HasValue);
+            Console.WriteLine(a != null);
+            Console.WriteLine(a.Value);
         }
     }
 }

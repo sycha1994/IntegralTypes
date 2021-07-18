18b85c5bba27244cc1d737ec616e41098419d71f
sycha1994 <sycha1994@gmail.com>
2021-07-18 22:38:03 +0900

    초기 커밋.

---
+++ MainApp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace IntegralTypes
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            sbyte a = -10;
+            byte b = 40;
+        }
+    }
+}
